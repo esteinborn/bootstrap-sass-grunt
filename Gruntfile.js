bd0f0096083d8391ba349d48c1a00a5ce0954231
Eric Steinborn <esteinborn@gmail.com>
2014-01-24 15:56:01 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,265 @@
+/* jshint node: true */
+/*!
+ * Bootstrap's Gruntfile
+ * http://getbootstrap.com
+ * Copyright 2013-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+module.exports = function (grunt) {
+  'use strict';
+
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+              ' * Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+              ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+              ' * Licensed under <%= _.pluck(pkg.licenses, "type") %> (<%= _.pluck(pkg.licenses, "url") %>)\n' +
+              ' */\n',
+    jqueryCheck: 'if (typeof jQuery === \'undefined\') { throw new Error(\'Bootstrap requires jQuery\') }\n\n',
+
+    // Task configuration.
+    clean: {
+      init: {
+        src: [
+          'bootstrap-sass',
+          'assets/css/_theme.scss'
+        ]
+      },
+      dist: {
+        src: 'dist'
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: 'configs/.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: 'assets/js/*.js'
+      }
+    },
+
+    csslint: {
+      options: {
+        csslintrc: 'configs/.csslintrc'
+      },
+      src: [
+        'dist/css/bootstrap.css',
+        'dist/css/bootstrap-theme.css',
+        'dist/css/main.css'
+      ]
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>\n<%= jqueryCheck %>',
+        stripBanners: false
+      },
+      bootstrap: {
+        src: [
+          'assets/js/transition.js',
+          'assets/js/alert.js',
+          'assets/js/button.js',
+          'assets/js/carousel.js',
+          'assets/js/collapse.js',
+          'assets/js/dropdown.js',
+          'assets/js/modal.js',
+          'assets/js/tooltip.js',
+          'assets/js/popover.js',
+          'assets/js/scrollspy.js',
+          'assets/js/tab.js',
+          'assets/js/affix.js'
+        ],
+        dest: 'dist/js/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      bootstrap: {
+        options: {
+          banner: '<%= banner %>',
+          report: 'min'
+        },
+        src: '<%= concat.bootstrap.dest %>',
+        dest: 'dist/js/<%= pkg.name %>.min.js'
+      }
+    },
+
+    // This is for Ruby-based SASS compilation
+    sass: {
+      dev: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'dist/css/<%= pkg.name %>.css': 'assets/css/bootstrap.scss',       // 'destination': 'source'
+          'dist/css/<%= pkg.name %>-theme.css': 'assets/css/theme.scss'
+        }
+      },
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed'
+        },
+        files: {                         // Dictionary of files
+          'dist/css/<%= pkg.name %>.css': 'assets/css/bootstrap.scss',       // 'destination': 'source'
+          'dist/css/<%= pkg.name %>-theme.css': 'assets/css/theme.scss'
+        }
+      }
+    },
+
+    //This is for LibSASS Support when it becomes available.
+    // sass: {
+    //   dev: {
+    //     options: {
+    //       includePaths: ['assets/stylesheets/'],
+    //       sourceComments: 'normal'
+    //     },
+    //     files: {
+    //       'dist/css/<%= pkg.name %>.css': 'assets/stylesheets/bootstrap.scss',
+    //       'dist/css/main.css': 'assets/stylesheets/main.scss',
+    //       'dist/css/<%= pkg.name %>-theme.css': 'assets/stylesheets/theme.scss'
+    //     }
+    //   },
+    //   dist: {
+    //     options: {
+    //       includePaths: ['assets/stylesheets/'],
+    //       outputStyle:'compressed'
+    //     },
+    //     files: {
+    //       'dist/css/<%= pkg.name %>.css': 'assets/stylesheets/bootstrap.scss',
+    //       'dist/css/main.css': 'assets/stylesheets/main.scss',
+    //       'dist/css/<%= pkg.name %>-theme.css': 'assets/stylesheets/theme.scss'
+    //     }
+    //   }
+    // },
+
+    usebanner: {
+      dist: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
+        },
+        files: {
+          src: [
+            'dist/css/<%= pkg.name %>.css',
+            'dist/css/<%= pkg.name %>.min.css',
+            'dist/css/<%= pkg.name %>-theme.css',
+            'dist/css/<%= pkg.name %>-theme.min.css',
+          ]
+        }
+      }
+    },
+
+    csscomb: {
+      sort: {
+        options: {
+          config: 'configs/.csscomb.json'
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.css': 'dist/css/<%= pkg.name %>.css',
+          'dist/css/<%= pkg.name %>-theme.css': 'dist/css/<%= pkg.name %>-theme.css'
+        }
+      }
+    },
+
+    copy: {
+      init: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bootstrap-sass/vendor/assets/',
+            src: ['fonts/**/*'],
+            dest: 'assets'
+          },
+          {
+            expand: true,
+            cwd: 'bootstrap-sass/vendor/assets/javascripts/bootstrap',
+            src: ['**/*'],
+            dest: 'assets/js'
+          },
+          {
+            expand: true,
+            cwd: 'bootstrap-sass/vendor/assets/stylesheets/bootstrap',
+            src: ['**/*'],
+            dest: 'assets/css'
+          },
+          {
+            expand: true,
+            cwd: 'bootstrap-sass/vendor/assets/stylesheets/bootstrap',
+            src: ['_theme.scss'],
+            dest: 'assets/css/',
+            rename: function(dest, src) {
+              return dest + 'theme.scss';
+            }
+          }
+        ]
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: './assets/fonts/',
+            src: ['**/*'],
+            dest: 'dist/css'
+          },
+          {
+            expand: true,
+            cwd: './assets/vendor/',
+            src: ['**/*'],
+            dest: 'dist/vendor'
+          }
+        ]
+      }
+    },
+
+    watch: {
+      src: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src']
+      },
+      sass: {
+        files: 'assets/css/*.scss',
+        tasks: 'sass'
+      }
+    }
+
+  });
+
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+  // JS development task.
+  grunt.registerTask('dev-js', ['concat']);
+
+  // JS distribution task.
+  grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+  // CSS development task.
+  grunt.registerTask('dev-css', ['sass:dev', 'csscomb', 'usebanner']);
+
+  // CSS distribution task.
+  grunt.registerTask('dist-css', ['sass:dist', 'usebanner']);
+
+  // Full development task.
+  grunt.registerTask('dev', ['clean:dist', 'dev-css', 'copy:dist', 'dev-js']);
+
+  // Full distribution task.
+  grunt.registerTask('dist', ['clean:dist', 'dist-css', 'copy:dist', 'dist-js']);
+
+  grunt.registerTask('init', ['copy:init', 'clean:init']);
+
+  // Default task.
+  grunt.registerTask('default', ['dev']);
+
+};
---
+++ README.md
@@ -0,0 +1,43 @@
+# Bootstrap SASS for Grunt
+
+The original version of [bootstrap-sass](https://github.com/twbs/bootstrap-sass) was built for Ruby users, and confused the crap out of me. I ported it to use a Grunt build process. I will also update when [libsass support gets fixed](https://github.com/twbs/bootstrap-sass/issues/494).
+
+This should hopefully be a drop and replace for new versions of boostrap-sass.
+
+## Bootstrap for Sass
+
+
+[![Build Status](https://secure.travis-ci.org/twbs/bootstrap-sass.png?branch=master)](http://travis-ci.org/twbs/bootstrap-sass)
+
+`bootstrap-sass` is a Sass-powered version of [Bootstrap](http://github.com/twbs/bootstrap), ready to drop right into your Sass powered applications.
+
+## Compiling CSS and JavaScript
+
+Bootstrap uses [Grunt](http://gruntjs.com/) with convenient methods for working with the framework. It's how we compile our code, run tests, and more. To use it, install the required dependencies as directed and then run some Grunt commands.
+
+### Install Grunt
+
+From the command line:
+
+1. Install `grunt-cli` globally with `npm install -g grunt-cli`.
+2. Navigate to the root `/bootstrap` directory, then run `npm install`. npm will look at [package.json](https://github.com/twbs/bootstrap/blob/master/package.json) and automatically install the necessary local dependencies listed there.
+
+When completed, you'll be able to run the various Grunt commands provided from the command line.
+
+**Unfamiliar with `npm`? Don't have node installed?** That's a-okay. npm stands for [node packaged modules](http://npmjs.org/) and is a way to manage development dependencies through node.js. [Download and install node.js](http://nodejs.org/download/) before proceeding.
+
+### Available Grunt commands
+
+#### Build Development code - `grunt`
+Run `grunt` to run tests locally and compile the CSS and JavaScript into `/dist`. **Uses [SASS](http://sass-lang.com/) and [JSHint](http://www.jshint.com/).**
+
+#### Build Production code - `grunt dist`
+`grunt dist` creates the `/dist` directory with compiled files. **Uses [SASS](http://sass-lang.com/) and [UglifyJS](http://lisperator.net/uglifyjs/).**
+
+#### Watch - `grunt watch`
+This is a convenience method for watching SASS & JavaScript files and automatically building them whenever you save.
+
+### Troubleshooting dependencies
+
+Should you encounter problems with installing dependencies or running Grunt commands, uninstall all previous dependency versions (global and local). Then, re-run `npm install`.
+
